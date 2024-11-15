45764e27e62edc4886cec7eb4d8d1fcc609e656f
Ale Pereira <naitwa-alexandre@gmail.com>
2024-11-15 09:47:47 -0300

    server up

---
+++ lesson/example02-creating-and-consuming-web-api/src/server.js
@@ -0,0 +1,39 @@
+import http from 'node:http';
+import { Readable } from 'node:stream';
+
+function* run() {
+  for (let index = 0; index <= 99; index++) {
+    const data = {
+      id: index,
+      name: `Alex-Andre-${index}`,
+      at: Date.now(),
+    }
+    yield data;
+  }
+}
+
+function handler(request, response) {
+  // const readableStream = Readable({
+  //   read() {
+  //     this.push('Hello\n')
+  //     this.push('World')
+  //     this.push(null)
+  //   }
+  // });
+
+    const readableStream = Readable({
+      read() {
+      for (const data of run()) {
+        this.push(JSON.stringify(data).concat("\n"))
+      }
+        this.push(null)
+      }
+  });
+
+  readableStream
+    .pipe(response);
+}
+
+http.createServer(handler)
+  .listen(3000)
+  .on('listening', () => console.log('Server running on port 3000'));
